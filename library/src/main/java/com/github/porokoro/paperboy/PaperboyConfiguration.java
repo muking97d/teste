75c6d5947f653e9b2ab20c24dd2c1d9ef127e0b0
porokoro <porokorodev@gmail.com>
2015-07-26 00:53:22 +0200

    The class PaperboyConfiguration is internal thus there is no need for it to be public

--- library/src/main/java/com/github/porokoro/paperboy/PaperboyConfiguration.java
+++ library/src/main/java/com/github/porokoro/paperboy/PaperboyConfiguration.java
@@ -20,11 +20,11 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.SparseArray;
 
-public class PaperboyConfiguration {
+class PaperboyConfiguration {
     private String                m_file;
     private int                   m_viewType;
-    private int m_sectionLayout;
-    private int m_typeLayout;
+    private int                   m_sectionLayout;
+    private int                   m_typeLayout;
     private int                   m_itemLayout;
     private boolean               m_sortItems;
     private SparseArray<ItemType> m_itemTypes;
